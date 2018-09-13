83c5f6516bfbbb1083a0b28aebe864ec11d4ab03
Zach Toben <zach.toben@gmail.com>
2018-09-12 20:19:24 -0500

    some css work, starting to make the pokedex

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,23 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import '../style/app.scss';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello world!</h1>
-      </div>
+      <Fragment>
+        <p className="page-header">react-dex</p>
+        <div className="pokedex-container">
+          <div className="left-screen">
+            <div className="top-border">
+            </div>
+            <div className="poke-display">
+              <h1>Hello world!</h1>
+            </div>
+          </div>
+          <div className="right-screen">
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }
